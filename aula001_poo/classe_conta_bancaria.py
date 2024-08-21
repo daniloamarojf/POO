db2d0c70af2dd2da86758a6c119aa60e24d66f34
daniloamarojf <daniloramarojg@gmail.com>
2024-08-20 21:35:32 -0300

    Criando classes

---
+++ aula001_poo/classe_conta_bancaria.py
@@ -0,0 +1,27 @@
+import os
+
+
+class ContaBancaria:
+    def __init__(self, numero_conta, nome_titular, saldo, agencia, tipo_conta):
+        self.numero_conta = numero_conta
+        self.nome_titular = nome_titular
+        self.saldo = saldo
+        self.agencia = agencia
+        self.tipo_conta = tipo_conta
+        
+        
+# Solicitando entrada de dados
+print('-'*70)
+os.system('cls')
+numero_conta = input('Digite o número da conta: ')
+nome_titular = input('Digite o nome do titular: ')
+saldo = float(input('Digite o saldo inicial: '))
+agencia = input('Digite a agência: ')
+tipo_conta = input('Digite o tipo de conta: ')
+
+# Criando um objeto da classe ContaBancaria com os dados fornecidos pelo usuário
+conta = ContaBancaria(numero_conta, nome_titular, saldo, agencia, tipo_conta)
+
+# Acessando e imprimindo atributos do objeto
+print('-'*70)
+print('\nInformações da Conta Bancária')
